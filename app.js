547aa1de9946f8f948958338cafa603800540952
Shyaboi <ian.shane.sears@gmail.com>
2020-08-30 11:01:30 -0700

    testing

--- app.js
+++ app.js
@@ -2,92 +2,147 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 require("electron-reload")(__dirname);
 // ./main.js
-const contextMenu = require('electron-context-menu');
-const { shell } = require('electron')
-const { exec, spawn } = require('child_process');
-
-
-
-
+const contextMenu = require("electron-context-menu");
+const { shell } = require("electron");
+const { exec, spawn } = require("child_process");
 
 // Add an item to the context menu that appears only when you click on an image
 // contextMenu({
-  // 	prepend: (params, browserWindow) => [{
-    //     label: 'Rainbow',
-    
-    // 		// Only show it when right-clicking images
-    // 		visible: params.mediaType === 'image'
-    //   },
-    //   {
-      //     role: "zoomIn"
-      //     // If you want to change the label "Zoom In"
-      //     // label: "Custom Zoom In Text"
-      // },
-      // {
-        //   role: "zoomOut"
-        //   // If you want to change the label "Zoom Out"
-        //   // label: "Custom Zoom Out Text"
-        // },
-        // {
-          //   role: "reload"
-          //   // If you want to change the label "Zoom Out"
-          //   // label: "Custom Zoom Out Text"
-          // },
-          // {
-            //   role: "selectAll"
-            //   // If you want to change the label "Zoom Out"
-            //   // label: "Custom Zoom Out Text"
-            // },
-            // ]
-            // });
-            
-            // Your code that starts a new application
-            let win;
-            (async () => {
-              await app.whenReady();
-            })();
-            
-            function createWindow() {
-              // Create the browser window.
-              // shell.openExternal('https://github.com')
-              shell.beep()
-              exec('chmod 755 mkdir.sh', (err, stdout, stderr) => {
-                if (err) {
-                  console.error(err);
-                  return;
-                }
-                console.log(stdout);
-              });
-              exec('mkdir.sh', (err, stdout, stderr) => {
-                if (err) {
-                  console.error(err);
-                  return;
-                }
-                console.log(stdout);
-              });
+// 	prepend: (params, browserWindow) => [{
+//     label: 'Rainbow',
 
-              exec('echo things', (err, stdout, stderr) => {
-                if (err) {
-                  console.error(err);
-                  return;
-                }
-                console.log(stdout);
-              });
-              // shell.openItem("./mkdir.sh")
-              const mainWindow = new BrowserWindow({
-                title: "Shyaboi",
-                icon: __dirname + "./sinbad.png",
-                // frame: false,
-                // titleBarStyle: 'customButtonsOnHover', 
-                transparent: true, frame: false ,
-                width: 450,
-                height: 100,
-                webPreferences: {
-                  preload: path.join(__dirname, "preload.js"),
-                },
-              });
-              
-              // and load the index.html of the app.
+// 		// Only show it when right-clicking images
+// 		visible: params.mediaType === 'image'
+//   },
+//   {
+//     role: "zoomIn"
+//     // If you want to change the label "Zoom In"
+//     // label: "Custom Zoom In Text"
+// },
+// {
+//   role: "zoomOut"
+//   // If you want to change the label "Zoom Out"
+//   // label: "Custom Zoom Out Text"
+// },
+// {
+//   role: "reload"
+//   // If you want to change the label "Zoom Out"
+//   // label: "Custom Zoom Out Text"
+// },
+// {
+//   role: "selectAll"
+//   // If you want to change the label "Zoom Out"
+//   // label: "Custom Zoom Out Text"
+// },
+// ]
+// });
+
+// Your code that starts a new application
+let win;
+(async () => {
+  await app.whenReady();
+})();
+
+function createWindow() {
+  // Create the browser window.
+  // shell.openExternal('https://github.com')
+  shell.beep();
+  console.log(process.platform);
+  const userOS = process.platform;
+  if (userOS === "win32" || "linux") {
+
+    exec("start mkdir.sh", (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+    });
+    // exec("echo you using wandows", (err, stdout, stderr) => {
+    //   if (err) {
+    //     console.error(err);
+    //     return;
+    //   }
+    //   console.log(stdout);
+    // });
+    // exec("node -v", (err, stdout, stderr) => {
+    //   if (err) {
+    //     console.error(err);
+    //     return;
+    //   }
+    //   const donuis = Number(stdout.slice(1, 3));
+    //   if (donuis >= 15) {
+    //     console.log("you got node, you good");
+    //   } else {
+    //     console.log("downloading node via curl");
+    //     var thing =
+    //       "curl -o node.msi 'http://dinguscrew.com:8080/Walls131498370195215.png'";
+    //     exec(
+    //       "curl -o node.msi https://nodejs.org/dist/v12.18.3/node-v12.18.3-x64.msi",
+    //       (err, stdout, stderr) => {
+    //         if (err) {
+    //           console.error(err);
+    //           return;
+    //         }
+    //         exec("start node.msi", (err, stdout, stderr) => {
+    //           if (err) {
+    //             console.error(err);
+    //             return;
+    //           }
+    //           console.log(stdout);
+    //         });
+    //         console.log(stdout);
+    //         exec("echo don node via curl", (err, stdout, stderr) => {
+    //           if (err) {
+    //             console.error(err);
+    //             return;
+    //           }
+    //           console.log(stdout);
+    //         });
+    //       }
+    //     );
+    //   }
+    //   console.log(typeof donuis);
+    // });
+  }
+  // exec('chmod 755 mkdir.sh', (err, stdout, stderr) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  //   console.log(stdout);
+  // });
+  // exec('mkdir.sh', (err, stdout, stderr) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  //   console.log(stdout);
+  // });
+
+  // exec('echo things', (err, stdout, stderr) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  //   console.log(stdout);
+  // });
+  // shell.openItem("./mkdir.sh")
+  const mainWindow = new BrowserWindow({
+    title: "Shyaboi",
+    icon: __dirname + "./sinbad.png",
+    // frame: false,
+    // titleBarStyle: 'customButtonsOnHover',
+    transparent: true,
+    frame: false,
+    width: 450,
+    height: 100,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // and load the index.html of the app.
   mainWindow.loadFile("index.html");
 
   // Open the DevTools.
