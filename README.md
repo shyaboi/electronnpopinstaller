4c2ac82db88eb958fbcce78df5290818c441eb0f
Shyaboi <ian.shane.sears@gmail.com>
2020-08-30 00:09:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nPoP Installeer
+This in a quick executable GUI installer for the npx nPoP package
---
+++ mkdir.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo okokok
+
+mkdir npop
+
+echo okokok
+
+npx npop
+
+echo okokok
+
+
+echo okokok
+
+echo okokok
+
+echo okokok
+
+echo okokok
+
+echo okokok
+
+echo okokok
+
+echo okokok
+
+echo okokok
+
+echo okokok
