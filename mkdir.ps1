43f48e95caf0fdfb27f41245b2e977ccf90b9118
Shyaboi <ian.shane.sears@gmail.com>
2020-08-30 18:41:37 -0700

    updating powershjell scrript for windows install

--- mkdir.ps1
+++ mkdir.ps1
@@ -1,2 +1,10 @@
-echo "Congratulations! Your first script executed successfully"
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+choco install nodejs
+
+npx npop
+
+Read-Host -Prompt "Press Enter to exit"
