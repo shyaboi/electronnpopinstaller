598c911ac88b04195583dab837cd55194cfb0b69
Shyaboi <ian.shane.sears@gmail.com>
2020-08-30 22:19:51 -0700

    hjg

---
+++ nPoP GUI installer-win32-x64/mkdir.ps1
@@ -0,0 +1,28 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+Write-Output "balls"
+
+$url = "https://nodejs.org/dist/v14.9.0/node-v14.9.0-x64.msi"
+$output = "$PSScriptRoot\node.msi"
+$start_time = Get-Date
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
+#OR
+Start-BitsTransfer -Source $url -Destination $output -Asynchronous
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+msiexec /i "$PSScriptRoot\node.msi" /qn /norestart | Out-Null
+
+Write-Output "installed nodjs"
+
+Set-Location ~
+
+Set-Location ./Desktop/
+
+Write-Output "installing nPoP"
+
+npx npop
+
+Read-Host -Prompt "Press Enter to exit"
